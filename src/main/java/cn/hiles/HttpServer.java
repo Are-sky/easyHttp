a3633ad3d8ae580cfda6e20a48111e0c0961b6e2
Helios <772389619@qq.com>
2023-04-17 21:57:14 +0800

    init

---
+++ src/main/java/cn/hiles/HttpServer.java
@@ -0,0 +1,15 @@
+package cn.hiles;
+
+/**
+ *
+ * description
+ * A Simple Framework for Implementing Http Services
+ * Driver class for HttpServer
+ * @author Helios
+ * Time：2023-04-17 20:40
+ */
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Server is running...");
+    }
+}
